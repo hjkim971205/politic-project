57cc897e67e9c854e6c261459d5044522175f316
Hyung Jin Kim <hyungjinkim1205@gmail.com>
2023-01-11 16:52:45 -0800

    change and separate logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,28 @@
+// User Interface Logic
+
 function hideResults() {
-    document.getElementById("conservative",).setAttribute("class", "hidden");
-    document.getElementById("liberal",).setAttribute("class", "hidden");
-}
-
-window.onload = function() {
-  hideResults();
-
+    document.getElementById("conservative").setAttribute("class", "hidden");
+    document.getElementById("liberal").setAttribute("class", "hidden");
+  }
+  
+  window.onload = function() {
+    hideResults();
+  
     document.querySelector("form").onsubmit = function(event) {
       event.preventDefault();
       hideResults();
-      
       const age = parseInt(document.querySelector("input#age").value);
-      const answer = document.querySelector("input#answer");
+      const question = document.querySelector("input#question");
   
-    if (age && answer) {
-      if (age <= 30 && answer === "no") {
-        document.getElementById("conservative").removeAttribute("class");
-      } else if (age >= 30 && answer === "yes") {
-        document.getElementById("conservative").removeAttribute("class");
-      } else if (age <= 30 && answer === "yes")
+      if (age && question) {
+        if (age <= 30 && answer === "no") {
+            document.getElementById("conservative").removeAttribute("class");
+        } else if (age > 30 && answer === "yes") {
+            document.getElementById("conservative").removeAttribute("class");
+        } else if (age <= 30 && answer === "yes")
+            document.getElementById("liberal").removeAttribute("class");
+    } else {
         document.getElementById("liberal").removeAttribute("class");
-      }  || age >= 30 && answer === "no") {
-        placement.removeAttribute("class");
-      }
     }
-}
+    };
+  };
