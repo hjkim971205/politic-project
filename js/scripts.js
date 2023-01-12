fa32110c58b923a65fb8b5d022b5cab4633b28bb
Hyung Jin Kim <hyungjinkim1205@gmail.com>
2023-01-11 16:29:44 -0800

    add if else statements to determine the placement on political meter

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,27 @@
+function hideResults() {
+    document.getElementById("conservative",).setAttribute("class", "hidden");
+    document.getElementById("liberal",).setAttribute("class", "hidden");
+}
+
 window.onload = function() {
-    const form = document.querySelector("form");
-    form.onsubmit = function(event) {
+  hideResults();
+
+    document.querySelector("form").onsubmit = function(event) {
       event.preventDefault();
+      hideResults();
+      
+      const age = parseInt(document.querySelector("input#age").value);
+      const answer = document.querySelector("input#answer");
+  
+    if (age && answer) {
+      if (age <= 30 && answer === "no") {
+        document.getElementById("conservative").removeAttribute("class");
+      } else if (age >= 30 && answer === "yes") {
+        document.getElementById("conservative").removeAttribute("class");
+      } else if (age <= 30 && answer === "yes")
+        document.getElementById("liberal").removeAttribute("class");
+      }  || age >= 30 && answer === "no") {
+        placement.removeAttribute("class");
+      }
     }
 }
